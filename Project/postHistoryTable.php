4bd4057154892287f5b0432488f33931488d5a0c
Ryan <kramer545@gmail.com>
2017-04-02 22:17:46 -0700

    added comment tracking to user profiles

--- Project/postHistoryTable.php
+++ Project/postHistoryTable.php
@@ -1,13 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<?php
-session_start();
-	if(!isset($_SESSION['userID']))
-	{
-		header("location: homepage.php");
-		die();
-	}
-?>
 <head>
 <title>360 Project</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
@@ -17,11 +9,20 @@ session_start();
 <body>
 <div id="container">
   <div id="banner">
-  <?php
+   <?php	
+   session_start();
+	if(!isset($_SESSION['userID']))
+	{
+		header("location: homepage.php");
+		die();
+	}
+	else
+	{
 		echo "<p id = \"loginLinks\">";
 		if($_SESSION['userID'] == "1")
 			echo "<a href=\"admin.php\">Admin</a> |";
 		echo "<a href=\"Profile.php\">Profile</a> | <a href=\"SignOut.php\">Sign Out</a></p>";
+	}
 	?>
 	<h1><a href = "homepage.php">www.BestDog.com</a></h1>
   </div>
@@ -56,51 +57,64 @@ session_start();
 	</form>
 	<h3>Most Searched</h3>
 	<ul>
-		<li><a href = "search.php">dog</a></li>
-		<li><a href = "search.php">best</a></li>
-		<li><a href = "search.php">good boy</a></li>
-		<li><a href = "search.php">aw</a></li>
+		<li><a href ="search.php">dog</a></li>
+		<li><a href ="search.php">best</a></li>
+		<li><a href ="search.php">good boy</a></li>
+		<li><a href ="search.php">aw</a></li>
 	</ul>
   </div>
   <div id="content">
-    <h2>Profile</h2>
-	<?php
-		$host = "cosc360.ok.ubc.ca";
+    <h2>Search</h2>
+    <hr noshade style = "border-width:0.10em">
+	<div>
+	<table>
+		<tr><th>Topic</th><th>Message</th><th>Time Posted
+		<?php 
+	
+			$searchTerm;
+			if(isset($_SESSION['userID']))
+			{
+				if($_SESSION['userID']=== '1')//admin gets more options
+					echo"</th><th>Edit</th><th>Delete";
+			}
+			echo"</th></tr>";
+			
+			$host = "cosc360.ok.ubc.ca";
 			$database = "db_24604143";
 			$user = "24604143";
-			$password = "24604143";  
+			$password = "24604143"; 
 
-		$connection = mysqli_connect($host, $user, $password, $database);
 
-		$error = mysqli_connect_error();
-		if($error != null)
-		{
-		  $output = "<p>Unable to connect to database!</p>";
-		  exit($output);
-		}
-		else
-		{
-			$sql = "SELECT * FROM user;";
-			$results = mysqli_query($connection, $sql);
+			$connection = mysqli_connect($host, $user, $password, $database);
 
-			//and fetch requsults
-			while ($row = mysqli_fetch_assoc($results))//search all users to see if there are already in database
+			$error = mysqli_connect_error();
+			if($error != null)
 			{
-			  if($row['ID'] === $_SESSION['userID'])
-			  {
-				  echo "<p>Username: ".$row['Username']."<br></p>";
-				  echo "<p>Email: ".$row['Email']." <a href = \"changeEmail.php\">Change Email</a><br></p>";
-				  echo "<p>Create Date: ".$row['CreateDate']."<br></p>";
-				  echo "<p><br><a href = \"changePassword.php\">Change Password</a></p>";
-				  echo '<p>Profile Pic: <img src="data:image/'.$row['ImageType'].';base64,'.base64_encode($row['Image']).' <a href = "changePic.php"><br></p>';
-			  }
+			  $output = "<p>Unable to connect to database!</p>";
+			  exit($output);
 			}
-		}
-    ?>
-	<hr noshade style = "border-width:0.10em">
-	<p><a href = "postHistoryTable.php">Post History</a></p>
+			else
+			{
+				$sql = "SELECT t.ID, t.Title, m.Post, m.CreateDate FROM thread t JOIN message m ON t.ID = m.ThreadID WHERE m.UserID = ".$_SESSION['userID'].""; //God this is sexy
+				$results = mysqli_query($connection, $sql);
+
+				//and fetch results
+				while ($row = mysqli_fetch_assoc($results))
+				{
+						echo "<tr><td><a href=\"Thread.php?threadID=".$row['ID']."\">".$row['Title']."</a></td><td>".$row['Post']."</td><td>".$row['CreateDate']."</td>";
+						if(isset($_SESSION['userID']))
+						{
+							if($_SESSION['userID'] === "1")
+								echo "<td><a href=\"editThread.php?threadID=".$row['ID']."\">Edit</a></td><td><a href=\"deleteThread.php?threadID=".$row['ID']."\">Delete</a></td>";
+						}
+						echo "</tr>";
+				}
+				echo "</table>";
+			}
+	?>
+	</div>
   </div>
-  <div id="footer"><a href="homepage.php">Home</a> | <a href="contactUs.php">contact</a> | Site By: Ryan Kramer | copyright stuff | filler| footer stuff</div>
+ <div id="footer"><a href="homepage.php">Home</a> | <a href="contactUs.php">contact</a> | Site By: Ryan Kramer | copyright stuff | filler| footer stuff</div>
 </div>
 </body>
-</php>
+</html>
